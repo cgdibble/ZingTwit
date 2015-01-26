852a5e381c91b7444d11f3201589fe807b3c7bb2
cgdibble <cgdibble@gmail.com>
2015-01-26 11:30:44 -0800

    removed old files

--- config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  url: 'mongodb://cgdibble:gemini1000@ds031721.mongolab.com:31721/zingtwit'
-}
--- public/js/controllers/indexCtrl.js
+++
@@ -1,4 +0,0 @@
-angular.module('IndexCtrl', [])
-  .controller('IndexController', function($scope) {
-    $scope.tagline = 'This is a tag line of some sort...';
-  });
