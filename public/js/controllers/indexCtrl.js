952c59309e39c7f47de3a192296da9537ed58784
cgdibble <cgdibble@gmail.com>
2015-01-26 15:15:17 -0800

    added their test/basic chart in a script on index, got it working. now do it in angular

--- public/js/controllers/indexCtrl.js
+++ public/js/controllers/indexCtrl.js
@@ -4,5 +4,16 @@ var app = angular.module("zingtwit", ["ngRoute", "ngDialog"])
       $http.get('/tags').success(function(data) {
         // take hash tags and make call to this route such that it passes in the
         // queried hashtags and calls Twitter API
+        var basicChart = {
+          "type":"line",
+          "title": {
+            "text":"Hashtag Trends"
+          },
+          "series": [
+            {"values": [6,3,4,5,6,9,14] },
+            {"values": [2,3,8,3,5,8,19] },
+            {"values": [1,5,2,7,9,11,22] }
+          ]
+        }
       })
    }]);
