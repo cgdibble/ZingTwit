7d165c46df7c7cd23607998561bc461cf4b7fe39
cgdibble <cgdibble@gmail.com>
2015-01-27 15:18:39 -0800

    all tokens and keys functioning from env

--- app/routes.js
+++ app/routes.js
@@ -4,8 +4,8 @@ var Twitter = require('twitter');
 var client = new Twitter({
   consumer_key : process.env.CONSUMER_KEY,
   consumer_secret : process.env.CONSUMER_SECRET,
-  access_token_key : '',
-  access_token_Secret : ''
+  access_token_key : process.env.ACCESS_TOKEN,
+  access_token_secret : process.env.ACCESS_TOKEN_SECRET
 });   // Need to add these values from twitter account but hidden in a DOTENV
 
 module.exports = function(app) {
@@ -24,6 +24,8 @@ module.exports = function(app) {
   app.put('/search', function(req, res) {
     console.log(process.env.CONSUMER_KEY);
     console.log(process.env.CONSUMER_SECRET);
+    console.log(process.env.ACCESS_TOKEN);
+    console.log(process.env.ACCESS_TOKEN_SECRET);
     // This route should take the data passed to it, parse it out properly such that it can then query
     //    the twitter db for users
     // take the data Twitter returns, parse out what is needed (followers, followed, tweets, retweets)
