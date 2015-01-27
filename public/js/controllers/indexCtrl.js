8970334a21272e848b69ed9f2b3f86ca927eabca
cgdibble <cgdibble@gmail.com>
2015-01-26 20:29:38 -0800

    updated dependencies, working controller from beginning

--- public/js/controllers/indexCtrl.js
+++ public/js/controllers/indexCtrl.js
@@ -1,4 +1,9 @@
+(function() {
+
 var dash = angular.module("zingTwit", []);
+
+
+
 
 // dash.config(['$routeProvider', function($routeProvider) {
 //   $routeProvider.when("/tags", {
@@ -9,23 +14,26 @@ var dash = angular.module("zingTwit", []);
 //   });
 // }])
 
-dash.controller('DashController', ['$scope', '$http', '$routeParams', 'ngDialog',
- function($scope, $http, $routeParams, ngDialog){
+// dash.controller('DashController', ['$scope', '$http', '$routeParams', 'ngDialog',
+//  function($scope, $http, $routeParams, ngDialog){
 
-    $http.get('/tags').success(function(data) {
-      // take hash tags and make call to this route such that it passes in the
-      // queried hashtags and calls Twitter API
-      var basicChart = {
-        "type":"line",
-        "title": {
-          "text":"Hashtag Trends"
-        },
-        "series": [
-          {"values": [6,3,4,5,6,9,14] },
-          {"values": [2,3,8,3,5,8,19] },
-          {"values": [1,5,2,7,9,11,22] }
-        ]
-      }
-    });
+//     $http.get('/tags').success(function(data) {
+//       // take hash tags and make call to this route such that it passes in the
+//       // queried hashtags and calls Twitter API
+//       var basicChart = {
+//         "type":"line",
+//         "title": {
+//           "text":"Hashtag Trends"
+//         },
+//         "series": [
+//           {"values": [6,3,4,5,6,9,14] },
+//           {"values": [2,3,8,3,5,8,19] },
+//           {"values": [1,5,2,7,9,11,22] }
+//         ]
+//       }
+//     });
 
- }]);
+//     alert("Hello!")
+//  }]);
+
+})();
