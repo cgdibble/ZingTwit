79ce2e66d31341ed6c199529ae6a6039b0251433
cgdibble <cgdibble@gmail.com>
2015-01-26 16:03:01 -0800

    making small changes to try and incorporate node and angular

--- public/js/controllers/indexCtrl.js
+++ public/js/controllers/indexCtrl.js
@@ -1,19 +1,31 @@
-var app = angular.module("zingtwit", ["ngRoute", "ngDialog"])
-  .controller('DashController', ['$scope', '$http', '$routeParams', 'ngDialog',
-   function($scope, $http, $routeParams, ngDialog){
-      $http.get('/tags').success(function(data) {
-        // take hash tags and make call to this route such that it passes in the
-        // queried hashtags and calls Twitter API
-        var basicChart = {
-          "type":"line",
-          "title": {
-            "text":"Hashtag Trends"
-          },
-          "series": [
-            {"values": [6,3,4,5,6,9,14] },
-            {"values": [2,3,8,3,5,8,19] },
-            {"values": [1,5,2,7,9,11,22] }
-          ]
-        }
-      })
-   }]);
+var dash = angular.module("zingTwit", []);
+
+// dash.config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.when("/tags", {
+//     controller:'DashController'//,
+//     // templateUrl: ''
+//   }).otherwise({
+//     redirectTo: '/tags'
+//   });
+// }])
+
+dash.controller('DashController', ['$scope', '$http', '$routeParams', 'ngDialog',
+ function($scope, $http, $routeParams, ngDialog){
+
+    $http.get('/tags').success(function(data) {
+      // take hash tags and make call to this route such that it passes in the
+      // queried hashtags and calls Twitter API
+      var basicChart = {
+        "type":"line",
+        "title": {
+          "text":"Hashtag Trends"
+        },
+        "series": [
+          {"values": [6,3,4,5,6,9,14] },
+          {"values": [2,3,8,3,5,8,19] },
+          {"values": [1,5,2,7,9,11,22] }
+        ]
+      }
+    });
+
+ }]);
