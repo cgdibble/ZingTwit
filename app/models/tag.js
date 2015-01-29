a8cc625b227c66b0abaa75aab895c175d40510c7
cgdibble <cgdibble@gmail.com>
2015-01-29 11:42:53 -0800

    added missing /send module

--- app/models/tag.js
+++
@@ -1,9 +0,0 @@
-// need to access the mongoose module
-
-var mongoose = require('mongoose');
-
-// define the user model in the DataBase!!
-module.exports = mongoose.model('Tag', {
-  hashtag : { type : String, default : '' },
-  count : { type : Number, default : 0}
-})
